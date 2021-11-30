8a0b23ece3d7d035c7c62dd27b23dbed02ea147f
alexander olhovik <just1pman@gmail.com>
2021-12-01 00:02:31 +0300

    add chakra ui

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
+import Chakra, { CThemeProvider, CReset } from '@chakra-ui/vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import globalStyles from '@/assets/styles/main.scss'
 
 Vue.config.productionTip = false
+Vue.use(Chakra)
 
 new Vue({
     router,
     store,
     globalStyles,
-    render: h => h(App)
+    render: (h) => h(CThemeProvider, [h(CReset), h(App)])
 }).$mount('#app')
